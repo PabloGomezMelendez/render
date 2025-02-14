552e614fe65b5ea260905b670f9bd170078962c2
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2025-02-14 17:17:37 +0100

    proyecto sin BBDD

---
+++ crud-docker/src/main/java/com/app/web/CrudDockerApplication.java
@@ -0,0 +1,13 @@
+package com.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDockerApplication.class, args);
+	}
+
+}
